a682b6f100f16d6ea0f4a1da825b53c295b117d8
romanovich3313 <124934750+romanovich3313@users.noreply.github.com>
2023-04-19 18:46:25 +0300

    create

---
+++ clean_folder/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clean_folder',
+    version='1',
+    description='You can clean your computer with this code',
+    url='https://github.com/Siia95/Clean_Folder',
+    author='Roman Palij',
+    author_email='roman.romanovich.pl@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={'console_scripts': ['clean_folder = clean_folder.clean_folder.clean:main']})
